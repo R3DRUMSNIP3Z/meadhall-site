b307b1753da03649c9a4b2e926f6f694692c46a2
R3DRUMSNIP3Z <154389389+R3DRUMSNIP3Z@users.noreply.github.com>
2025-09-20 22:25:44 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        account: resolve(__dirname, "account.html"),
+        friends: resolve(__dirname, "friends.html"),
+        friendprofile: resolve(__dirname, "friendprofile.html"),
+        profile: resolve(__dirname, "profile.html"),
+      },
+    },
+  },
+  server: { host: true, port: 5173 },
+  preview: { host: true, port: 5173 },
+});
+
 
