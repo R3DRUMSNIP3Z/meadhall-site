e80df18a55e455bc21042317fbf24b18e0da8e13
R3DRUMSNIP3Z <154389389+R3DRUMSNIP3Z@users.noreply.github.com>
2025-09-20 22:29:09 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [],
   build: {
     rollupOptions: {
       input: {
@@ -15,8 +13,6 @@ export default defineConfig({
       },
     },
   },
-  server: { host: true, port: 5173 },
-  preview: { host: true, port: 5173 },
 });
 
 
