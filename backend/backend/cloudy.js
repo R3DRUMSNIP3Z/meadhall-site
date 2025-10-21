5fbd6eaab3fb1fb881830e21cb4de7ec20c15eb0
R3DRUMSNIP3Z <toki062789@gmail.com>
2025-10-21 01:24:11 -0400

    Deploy local changes

--- backend/backend/cloudy.js
+++ backend/backend/cloudy.js
@@ -1,10 +1,14 @@
 // backend/cloudy.js
-const { v2: cloudinary } = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "Root",
-  api_key:    process.env.CLOUDINARY_API_KEY || "112443578766182",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "DPPzCcW27glpmhS5SEslcLdm1sU",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // from .env
+  api_key:    process.env.CLOUDINARY_API_KEY,    // from .env
+  api_secret: process.env.CLOUDINARY_API_SECRET, // from .env
 });
 
-module.exports = { cloudinary };
+module.exports = { cloudinary, CloudinaryStorage, multer };
+
+
