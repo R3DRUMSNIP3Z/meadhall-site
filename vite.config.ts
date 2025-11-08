206cc61fbfecbfbc8adc48634421fc481f7d3eda
R3DRUMSNIP3Z <toki062789@gmail.com>
2025-11-08 02:20:14 -0500

    Deploy local changes

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,8 @@ const inputs: Record<string, string> = {
   game:          html("game.html")!,        // ← ✅ add this line
   shop:          html("shop.html")!,   // ← add this line
   brisingrshop:  html("brisingrshop.html")!,  // ✅ Add this line
+    quests:        html("quests.html")!,        // ✅ add this line for Quests page
+
 
 
 };
